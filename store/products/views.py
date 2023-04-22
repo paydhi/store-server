60b1504bdb8bfc55c470caf92e294ee077a74fd4
Paydhi <paydhi@gmail.com>
2023-04-22 14:12:20 +0600

    added /products/ page

--- store/products/views.py
+++ store/products/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
