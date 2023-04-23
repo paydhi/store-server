03bd7645cfa0ec6aa1e75a2371c562e0bb40cdc1
Paydhi <paydhi@gmail.com>
2023-04-23 13:41:22 +0600

    call of ProductCategory returns name field, added categories in ProductCategory table

--- store/products/models.py
+++ store/products/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
