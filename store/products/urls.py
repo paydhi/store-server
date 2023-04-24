82fe754852dfd50e8e338c0f0bb281f6a8b34736
Paydhi <paydhi@gmail.com>
2023-04-24 13:59:59 +0600

    added urls.py from products app to main urls.py

--- store/products/urls.py
+++ store/products/urls.py
@@ -14,18 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 
-from products.views import index
+from products.views import products
+
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
+    path('', products, name='index'),
     ]
-
-if settings.DEBUG == True:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
